9aa934623fca1acfcd607392b39d85287e7c0e8f
Maria Wuertz <bitrikova.mariia@web.de>
2021-12-11 21:56:44 +0100

    ADDs content to RecipePage.vue

--- src/components/features/recipe-page/RecipePage.vue
+++ src/components/features/recipe-page/RecipePage.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>{{ currentRecipe.name }}</div>
+  <div>
+    <v-container>
+      <v-row
+        class="
+          display__flex
+          flex-wrap-reverse
+          align-start align-sm-center
+          justify-sm-center
+        "
+      >
+        <v-col
+          sm="12"
+          md="6"
+          class="display__flex flex-column align-start justify-md-start"
+          ><v-app-bar-title>{{ recipe.name }}</v-app-bar-title>
+          <div class="ml-5 mt-3">Zubereitung: {{ recipe.time }}</div>
+          <div class="ml-5 mt-3">
+            Schwierigkeitsgrad: {{ recipe.levelOfDifficulty }}
+          </div>
+        </v-col>
+        <v-col sm="12" md="6">
+          <v-img
+            height="350"
+            :src="recipe.imageLink"
+            class="dashboard__image__style"
+          ></v-img
+        ></v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 <style>
 @import "../../../assets/styles.css";
@@ -9,22 +38,22 @@ import { replaceMutatedVowel } from "@/shared/replaceMutatedVowel";
 
 export default {
   data() {
-    return {};
+    return {
+      recipe: null,
+    };
   },
-  computed: {
-    currentRecipe() {
-      const storage = this.$store.getters.getCurrentRecipes;
-      const splitUrl = this.$router.currentRoute.value.fullPath
-        .toString()
-        .split("/");
-      const splitRecipeName = splitUrl[splitUrl.length - 1]
-        .split("-")
-        .join(" ");
-      return storage.find(
-        (item) =>
-          replaceMutatedVowel(item.name).toLowerCase() === splitRecipeName
-      );
-    },
+  beforeMount() {
+    const storage = this.$store.getters.getCurrentRecipes;
+    const splitUrl = this.$router.currentRoute.value.fullPath
+      .toString()
+      .split("/");
+    const splitRecipeName = splitUrl[splitUrl.length - 1].split("-").join(" ");
+    this.recipe = storage.find(
+      (item) =>
+        replaceMutatedVowel(item.name).toLowerCase().split("-").join(" ") ===
+        splitRecipeName
+    );
+    console.log(this.recipe);
   },
   mounted() {},
 };
