fb5c699a6014bc27c0177e491750708ffbd0688f
Maria Wuertz <bitrikova.mariia@web.de>
2021-12-12 13:44:38 +0100

    Adds all ingredient to a form with submit

--- src/components/features/recipe-page/RecipePage.vue
+++ src/components/features/recipe-page/RecipePage.vue
@@ -52,14 +52,25 @@
             {{ this.portionNumber === 1 ? "Portion" : "Portionen" }}</span
           ><v-btn class="mr-1 ml-4" @click="incrementPortionNumber">+</v-btn>
         </div>
-        <div v-for="ingredient in recipe.ingredients" :key="ingredient.name">
-          <v-checkbox
-            hide-details
-            color="indigo"
-            :label="`${ingredient.mass * this.portionNumber} ${ingredient.unity}
-          ${ingredient.name}`"
-          ></v-checkbox>
-        </div>
+        <form>
+          <div v-for="ingredient in recipe.ingredients" :key="ingredient.name">
+            <v-checkbox
+              hide-details
+              color="indigo"
+              :value="ingredient.name"
+              :id="'ingredient' + index"
+              :label="`${
+                ingredient.mass * this.portionNumber === 0
+                  ? ''
+                  : ingredient.mass * this.portionNumber
+              } ${ingredient.unity}  ${ingredient.name}`"
+              v-model="selection.ingredients"
+            />
+          </div>
+        </form>
+        <v-btn type="submit" @click="submit" class="mt-3 mb-5"
+          >Zum Einkaufswagen hinzufügen</v-btn
+        >
       </v-row>
     </v-container>
   </div>
@@ -75,6 +86,9 @@ export default {
     return {
       recipe: null,
       portionNumber: 1,
+      selection: {
+        ingredients: [],
+      },
     };
   },
   beforeMount() {
@@ -88,8 +102,15 @@ export default {
         replaceMutatedVowel(item.name).toLowerCase().split("-").join(" ") ===
         splitRecipeName
     );
+    this.selection.ingredients = this.recipe.ingredients.map(
+      (item) => item.name
+    );
+    console.log(this.selection);
   },
   methods: {
+    submit() {
+      console.log(this.selection.ingredients); // TODO shopping cart
+    },
     incrementPortionNumber() {
       this.portionNumber += 1;
     },
