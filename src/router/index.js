72b4f178c6aa8f32823884af27757985469e9177
Maria Wuertz <bitrikova.mariia@web.de>
2021-12-12 16:21:24 +0100

    ADDs redirect in case of false url

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,28 @@ const routes = [
     name: "rezepte",
     component: () => import("../components/features/recipe-page/RecipePage"),
   },
+  // plan on directly navigating to the not-found route using its name
+  {
+    path: "/:pathMatch(.*)*",
+    name: "not-found",
+    beforeEnter(to, from, next) {
+      next({
+        path: "/",
+        replace: true,
+      });
+    },
+  },
+  // if you omit the last `*`, the `/` character in params will be encoded when resolving or pushing
+  {
+    path: "/:pathMatch(.*)",
+    name: "bad-not-found",
+    beforeEnter(to, from, next) {
+      next({
+        path: "/",
+        replace: true,
+      });
+    },
+  },
 ];
 
 const router = createRouter({
