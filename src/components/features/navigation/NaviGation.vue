98b6740ddc025658cf5e49e56b9ed5ea259fda7f
Maria Wuertz <bitrikova.mariia@web.de>
2021-12-12 16:27:37 +0100

    IMPROVEMENTS of navigation

--- src/components/features/navigation/NaviGation.vue
+++ src/components/features/navigation/NaviGation.vue
@@ -1,23 +1,34 @@
 <template>
   <v-app-bar density="compact">
-    <v-app-bar-title>TheChef</v-app-bar-title>
+    <v-app-bar-title>
+      <router-link :to="{ name: 'home' }" class="routing-link"
+        >TheChef</router-link
+      ></v-app-bar-title
+    >
     <v-spacer></v-spacer>
     <v-btn class="mr-3" text>
-      <router-link :to="{ name: 'home' }">Dashboard</router-link></v-btn
+      <router-link :to="{ name: 'home' }" class="routing-link"
+        >Dashboard</router-link
+      ></v-btn
     >
     <v-btn class="mr-3" text
-      ><router-link :to="{ name: 'rezept' }"
+      ><router-link :to="{ name: 'rezept' }" class="routing-link"
         >Rezepte anlegen</router-link
       ></v-btn
     >
     <v-btn class="mr-3" text>
-      <router-link :to="{ name: 'einkaufswagen' }"
+      <router-link :to="{ name: 'einkaufswagen' }" class="routing-link"
         >Einkaufswagen</router-link
       ></v-btn
     >
     <v-divider inset vertical></v-divider>
-    <v-btn class="mr-4 ml-5" color="primary" plain>
-      <v-icon left icon="mdi-handshake-outline"></v-icon>
+    <v-btn class="mr-4 ml-5" color="#348d9a" plain>
+      <v-icon
+        left
+        class="icon-color"
+        color="white"
+        icon="mdi-handshake-outline"
+      ></v-icon>
     </v-btn>
   </v-app-bar>
 </template>
