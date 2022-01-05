1c535c97f7b2907456aebda8e69e0a6fe2475fad
Maria Wuertz <bitrikova.mariia@web.de>
2022-01-06 00:04:22 +0100

    ADDs first form fields for recipe creation

--- src/views/CreateRecipe.vue
+++ src/views/CreateRecipe.vue
@@ -1,20 +1,15 @@
 <template>
   <div>
-    <v-app class="view-container">
-      <NewRecipe />
-    </v-app>
     <recipe-creation></recipe-creation>
   </div>
 </template>
 
 <script>
-import NewRecipe from "@/components/features/new-recipe/NewRecipe";
 import RecipeCreation from "@/components/features/ReceipeEditorial/components/RecipeCreation";
 
 export default {
   name: "CreateRecipe",
   components: {
-    NewRecipe,
     RecipeCreation,
   },
 };
